898d343d664b5b5c53ea7236dd0c28a2983b35fd
Raphael Beltrand <raphael@mooveo.co>
2022-03-22 16:51:09 +0100

    remove workflow

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
-          :rememberable, :validatable, :trackable
+          :rememberable, :validatable, :trackable, :lockable
     self.primary_key = :uuid
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
